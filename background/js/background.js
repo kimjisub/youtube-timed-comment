e48f188a57c2991b593db2dd4c3e1c715a563d30
김지섭 <0226daniel@gmail.com>
2021-08-27 22:55:53 +0900

    chore: 코드 정렬

--- background/js/background.js
+++ background/js/background.js
@@ -1,21 +1,22 @@
-const youtubes = {}
+const youtubes = {};
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	//console.log(changeInfo);
+	console.log(changeInfo);
 
-	if (changeInfo.status == 'complete'){
-		if(tab.url.includes('youtube.com/watch')){
-			youtubes[tabId]?.destructor()
-			youtubes[tabId] = new YoutubeApi(tab)
+	if (changeInfo.status == 'complete') {
+		if (tab.url.includes('youtube.com/watch')) {
+			youtubes[tabId]?.destructor();
+			youtubes[tabId] = new YoutubeApi(tab);
 		}
-		return
-	} 
+		return;
+	}
 
 	//console.log(tabId, 'tab changed', changeInfo)
-})
+});
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.videoChanged) youtubeApi.videoChanged(sender.tab, request.videoChanged)
-	if (request.scriptLoaded) youtubeApi.scriptLoaded(sender.tab)
-})
+	if (request.videoChanged)
+		youtubeApi.videoChanged(sender.tab, request.videoChanged);
+	if (request.scriptLoaded) youtubeApi.scriptLoaded(sender.tab);
+});
 
 //window.youtubeApi = youtubeApi
--- background/js/youtubeApi.js
+++ background/js/youtubeApi.js
@@ -3,12 +3,12 @@ class YoutubeApi {
 		this.tab = tab;
 		this.url = tab.url;
 		this.videoId = new URLSearchParams(new URL(this.url).search).get('v');
-		this.active = false
-        this.log('New Youtube', this.url);
+		this.active = false;
+		this.log('New Youtube', this.url);
 
-        this.loadApp(()=>{
-            this.active = true
-        })
+		this.loadApp(() => {
+			this.active = true;
+		});
 	}
 
 	destructor() {
@@ -16,26 +16,31 @@ class YoutubeApi {
 	}
 
 	loadApp(callback) {
-        this.loadScript({
-            code: `extensionId = '${chrome.runtime.id}'`,
-        }, () => {
-            this.loadScript({file: `background/js/script/yt-comment-scraper.js`}, ()=>{
-				this.loadScript({file: `background/js/script/chart.js`}, ()=>{
-               		this.loadScript({file: `background/js/script/index.js`}, callback)
-				})
-            })
-        })
+		this.loadScript(
+			{
+				code: `extensionId = '${chrome.runtime.id}'`,
+			},
+			() => {
+				this.loadScript(
+					{ file: `background/js/script/yt-comment-scraper.js` },
+					() => {
+						this.loadScript({ file: `background/js/script/chart.js` }, () => {
+							this.loadScript(
+								{ file: `background/js/script/index.js` },
+								callback
+							);
+						});
+					}
+				);
+			}
+		);
 	}
 
-    loadScript(script, callback) {
-        chrome.tabs.executeScript(
-            this.tab.id,
-            script,
-            callback
-        );
-    }
+	loadScript(script, callback) {
+		chrome.tabs.executeScript(this.tab.id, script, callback);
+	}
 
-	sendMessage( data) {
+	sendMessage(data) {
 		chrome.tabs.executeScript(this.tab.id, {
 			code: `window.postMessage(${JSON.stringify(data)}, '*')`,
 		});
