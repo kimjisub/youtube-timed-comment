a4211619b17f9afd3524b368e422a3c16eef4410
김지섭 <0226daniel@gmail.com>
2021-08-29 15:36:29 +0900

    feat: gausian 그래프 적용

--- script/chart-math.js
+++ script/chart-math.js
@@ -1,3 +1,10 @@
+// a 최고점의 높이
+// b 최고점의 위치
+// c 퍼진 정도
+function gaussian(x, a, b, c) {
+	return a * Math.exp((-((x - b) ** 2) / 2) * c ** 2);
+}
+
 function toHistogram(data, { size, min, max }) {
 	let cmin = Infinity;
 	let cmax = -Infinity;
@@ -33,7 +40,7 @@ function applyAmbientWeights(list, around) {
 		for (let j = start; j <= end; j++) {
 			const diff = Math.abs(i - j);
 			const diffRatio = diff / around || 0;
-			const weight = 1 - diffRatio ** 2;
+			const weight = gaussian(diffRatio, 1, 0, 3.5);
 			if (list[j] > 0) {
 				sum += weight * list[j];
 				count++;
